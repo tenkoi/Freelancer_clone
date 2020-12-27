8b1f5abab9a7e1d68f36966cd42c1f6f11b9718d
Faiz <tenkois@gmail.com>
2020-12-28 03:05:21 +0800

    T3: Update User edit page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, 
               path: '', 
               path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
-              controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+              controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
 end
